f3e9da771f94772ab32c42deadfe4c922a52a9fd
KarenF <karenf@outlook.com>
2020-02-26 15:25:29 -0300

    add repository

--- src/main/java/br/com/java/listaEventos/entity/Evento.java
+++ src/main/java/br/com/java/listaEventos/entity/Evento.java
@@ -1,13 +1,26 @@
 package br.com.java.listaEventos.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.Data;
 
 @Entity
 @Data
-public class Evento {
+public class Evento implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2339241340877385457L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String nome;
 	private String local;
 	private String data;
---
+++ src/main/resources/db/migration/postgresql/V1__init.sql
@@ -0,0 +1,7 @@
+create table eventos(
+id serial,
+nome varchar(50),
+local varchar(50),
+data varchar(50),
+horario varchar(50),
+primary key (id));
