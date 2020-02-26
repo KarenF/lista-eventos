748508405e8d2b7eee6338e5fbaeee7873b99a9a
KarenF <karenf@outlook.com>
2020-02-26 15:33:44 -0300

    add post and get controller

--- src/main/java/br/com/java/listaEventos/controller/EventoController.java
+++ src/main/java/br/com/java/listaEventos/controller/EventoController.java
@@ -1,13 +1,27 @@
 package br.com.java.listaEventos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.java.listaEventos.entity.Evento;
+import br.com.java.listaEventos.repository.EventoRepository;
 
 @Controller
 public class EventoController {
 
-	@RequestMapping("/cadastrarEvento")
+	@Autowired
+	private EventoRepository repository;
+
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
 	public String form() {
 		return "evento/formEvento";
+	}
+	
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
+	public String form(Evento evento) {
+		repository.save(evento);
+		return "redirect:/cadastrarEvento";
 	}
 }
