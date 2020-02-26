9f80572d8935cf22b7f65c3782b7c91954569e36
KarenF <karenf@outlook.com>
2020-02-26 09:29:17 -0300

    add project

---
+++ src/main/java/br/com/java/listaEventos/ListaEventosApplication.java
@@ -0,0 +1,13 @@
+package br.com.java.listaEventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/java/listaEventos/ListaEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java.listaEventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
